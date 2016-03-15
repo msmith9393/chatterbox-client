256ee115585a80882e1b38484c03fa84ff772973
msmith9393 <megan@tokaypress.com>
2016-03-14 19:21:32 -0700

    Added config file to specRunner.html

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,23 +1,20 @@
 var app = {};
-
+app.server = 'https://api.parse.com/1/classes/messages';
 app.init = function() {
-  app.fetch();
 };
 
 //Send posts to other users
 app.send = function(message) {
-  // debugger;
   $.ajax({
-    'url': 'https://api.parse.com/1/classes/messages',
-    'type': 'POST',
-    'data': JSON.stringify(message),
-    'contentType': 'application/json',
-    'success': function(response) {
-      console.log(response);
+    url: app.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function(response) {
+      console.log('Successful Post');
     },
-    'error': function(data) {
-      console.log(data);
-      $('#chats').text('An error has occurred');
+    error: function(data) {
+      console.log('An error has occurred Post');
     }
   });
 };
@@ -25,15 +22,18 @@ app.send = function(message) {
 //Get posts from other users
 app.fetch = function() {
   $.ajax({
-    'url': 'https://api.parse.com/1/classes/messages',
-    'data': {
+    url: app.server,
+    type: 'GET',
+    data: {
       format: 'json'
     },
-    'error': function() {
-      $('#chats').text('An error has occurred');
+    error: function(data) {
+      console.log('An error has occurred Get');
+      console.log(data);
     },
-    dataType: 'json',
     success: function(data) {
+      console.log('Successful Get');
+      console.log(data);
       var userName, date, message;
       var res = data.results;
       for (var i = 0; i < res.length; i++) {
@@ -46,29 +46,28 @@ app.fetch = function() {
         message = res[i].text;
         app.post(userName, date, message);
       }
-    },
-    type: 'GET'
+    }
   });
 };
 
 
+app.post = function (userName, date, message) {
+  var $msgBox = $('<div class="msg-box"></div>');
+  var $userName = $('<h2 class="user-name"></h2>');
+  var $date = $('<p class="date"></p>');
+  var $message = $('<p class="message"></p>');
+  $userName.text(userName);
+  $date.text(date);
+  $message.text(message);
+  $msgBox.append($userName).append($date).append($message);
+  app.$chats.prepend($msgBox);
+};
+
+
 $(function() {
-  var $chats = $('#chats');
-
-  app.post = function (userName, date, message) {
-    var $msgBox = $('<div class="msg-box"></div>');
-    var $userName = $('<h2 class="user-name"></h2>');
-    var $date = $('<p class="date"></p>');
-    var $message = $('<p class="message"></p>');
-    $userName.text(userName);
-    $date.text(date);
-    $message.text(message);
-    $msgBox.append($userName).append($date).append($message);
-    $chats.prepend($msgBox);
-  };
-
-  $('[name="submit-btn"]').click(function(e) {
-    // debugger;
+  app.$chats = $('#chats');
+  app.fetch();
+  $('[name="submit-btn"]').on('click', function(e) {
     e.preventDefault();
     var userName = window.location.search.split('username=')[1];
     var date = new Date();
@@ -79,26 +78,9 @@ $(function() {
       roomname: roomName
     };
     app.send(message);
-    // post(userName, date, text);
   });
-  // app.send();
-  app.fetch();
+
+
 });
-// $(function() {
-//   var $chats = $('#chats');
-  
 
 
-//   var post = function (userName, date, message) {
-//     var $msgBox = $('<div class="msg-box"></div>');
-//     var $userName = $('<h2 class="user-name"></h2>');
-//     var $date = $('<p class="date"></p>');
-//     var $message = $('<p class="message"></p>');
-//     $userName.text(userName);
-//     $date.text(date);
-//     $message.text(message);
-//     $msgBox.append($userName).append($date).append($message);
-//     $chats.prepend($msgBox);
-//   };
-
-// });
